fe1b75f4682a0998a208d732ccdf5484506c4f48
Алекс <acamukhin@gmail.com>
2014-03-27 00:21:33 +0700

    Умножение

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,7 +9,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	printf("Hello, World");
 	int a, b;
 	scanf_s("%d%d", &a, &b);
-	printf("%d %d %d\n", a, b, a + b);
+	printf("%d %d %d\n", a, b, a * b);
 	_getch();
 	return 0;
 }
